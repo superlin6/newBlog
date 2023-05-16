13c21a6def259a7490ced6623d0bf5b5dc6dd74a
lindehao <lindehao@4399.com>
2023-05-16 17:41:11 +0800

    feat: uninstall viewUI

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index'
-import ViewUIPlus from 'view-ui-plus'
-import 'view-ui-plus/dist/styles/viewuiplus.css'
 import animate from "animate.css";
 import '../src/assets/css/main.less'
 import '../src/assets/css/fonts.less'
@@ -13,5 +11,5 @@ import 'element-plus/theme-chalk/el-message.css'
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(router).use(animate).use(ViewUIPlus).use(pinia)
+app.use(router).use(animate).use(pinia)
 app.mount('#app')
